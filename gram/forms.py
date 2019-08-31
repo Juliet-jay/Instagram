b8c4c304876cfb5093a65367392089b718b2a8ac
kathure <juliekmuthee@gmail.com>
2019-08-31 18:09:47 +0300

    add user form

--- gram/forms.py
+++ gram/forms.py
@@ -6,6 +6,12 @@ class PostForm(forms.ModelForm):
   class Meta:
     model = Image
     fields = ('image_caption', 'image', 'tag_someone',)
+    
+class UserForm(forms.ModelForm):
+    class Meta:
+    model = User
+    fields = ('first_name', 'last_name', 'email')
+
 
 class SignUpForm(forms.ModelForm):
   class Meta:
