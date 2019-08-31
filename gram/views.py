786dcd60cc6da2c725313bb0ab25724293341e06
kathure <juliekmuthee@gmail.com>
2019-08-31 17:33:46 +0300

    add def upload function

--- gram/views.py
+++ gram/views.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 from .models import Profile, Image
 from django.contrib.auth.models import User
 from . import models
-from annoying.decorators import ajax_request
+# from annoying.decorators import ajax_request
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 
@@ -24,4 +24,30 @@ def index(request):
 
 def explore(request):
     return render(request, 'user_list.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def logout(request):
+    return render(request, 'registration/logout.html')
+
+
+def login(request):
+    return render(request, 'registration/login.html')
+    
+
+def upload(request):
+    current_user = request.user
+    p = Profile.objects.filter(id=current_user.id).first()
+    imageuploader_profile = Image.objects.filter(imageuploader_profile=p).all()
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.imageuploader_profile= p
+            post.save()
+            return redirect('index')
+    else:
+        form =PostForm
+    return render(request, 'post_pic.html', {"form": form})
 
