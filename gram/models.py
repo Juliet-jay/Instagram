9dcb74382cf6f2cd5cba4c000bb9cf71edab734b
kathure <juliekmuthee@gmail.com>
2019-08-31 15:29:04 +0300

    add class image

--- gram/models.py
+++ gram/models.py
@@ -14,3 +14,45 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='ProfilePicture/',default=True)
     profile_avatar = models.ImageField(upload_to='AvatorPicture/',default=True)
     date = models.DateTimeField(auto_now_add=True, null= True)  
+    
+    def __str__(self):
+            return self.profile.user
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self,cls):
+        cls.objects.get(id = self.id).delete()
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to ='pictsagram/',default='Tony')
+    image_caption = models.CharField(max_length=700 ,default=True)
+    tag_someone = models.CharField(max_length=50,blank=True)
+    imageuploader_profile = models.ForeignKey(User, on_delete=models.CASCADE,null='True', blank=True)
+    image_likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
+    date = models.DateTimeField(auto_now_add=True, null= True)
+    
+    def __str__(self):
+        return self.image_caption
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self,cls):
+        cls.objects.get(id = self.id).delete()
+
+
+class Comments (models.Model):
+    comment_post = models.CharField(max_length=150)
+    author = models.ForeignKey('Profile',related_name='commenter' , on_delete=models.CASCADE)
+    commented_image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.author
+    
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self,cls):
+        cls.objects.get(id = self.id).delete()
+        
